3117bb7db32aa3886b231f3506ee21d92f91bae1
meenuteneti <87061873+meenuteneti@users.noreply.github.com>
2021-09-23 18:30:30 +0530

    Add files via upload

---
+++ size.cpp
@@ -0,0 +1,30 @@
+// C program to find the size of int, char,
+// float and double data types
+
+#include <stdio.h>
+
+int main()
+{
+	int integerType;
+	char charType;
+	float floatType;
+	double doubleType;
+
+	
+	printf("Size of int is: %ld\n",
+		sizeof(integerType));
+
+	printf("Size of char is: %ld\n",
+		sizeof(charType));
+
+
+	printf("Size of float is: %ld\n",
+		sizeof(floatType));
+
+
+	printf("Size of double is: %ld\n",
+		sizeof(doubleType));
+
+	return 0;
+}
+
